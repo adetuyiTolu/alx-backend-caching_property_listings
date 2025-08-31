87ab5281dc32f7e01590df3b19f796f8d06daa4a
Tolu Adetuyi <tolu.adetuyi@gmail.com>
2025-08-31 18:27:53 -0400

    update listing

--- alx_backend_caching_property_listings/settings.py
+++ alx_backend_caching_property_listings/settings.py
@@ -88,7 +88,7 @@ DATABASES = {
 CACHES = {
     "default": {
         "BACKEND": "django_redis.cache.RedisCache",
-        "LOCATION": "redis://redis:6379/1",  # matches docker-compose service name
+        "LOCATION": "redis://127.0.0.1:6379/1",  # matches docker-compose service name
         "OPTIONS": {
             "CLIENT_CLASS": "django_redis.client.DefaultClient",
         }
